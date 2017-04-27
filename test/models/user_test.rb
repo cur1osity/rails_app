6bd88cab9cdfbf6eb05a9b028b7986b498d036d4
cur1osity <marcin@cur1osity.com>
2017-04-27 11:01:30 +0200

    Add account activation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -76,8 +76,8 @@ end
   end
 
  test "authenticated? should return false for a user with nil digest" do
-   assert_not @user.authenticated?('')
-   end
+    assert_not @user.authenticated?(:remember, '')
+  end
 
 
 end
