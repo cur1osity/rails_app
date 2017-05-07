0e9e529bc0de2d0be09bee156626bc937037ce9c
cur1osity <marcin@cur1osity.com>
2017-05-07 19:03:09 +0200

    Add user following

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,6 @@ module SampleApp
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.action_controller.include_all_helpers = true
+    # Include the authenticity token in remote forms.
   end
 end
